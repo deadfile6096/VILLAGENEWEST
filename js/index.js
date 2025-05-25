c45d6a3272498146dc9c8fffc4d33249d8f158d1
deadfile6096 <fap96@yandex.ru>
2025-05-25 22:08:30 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 document.getElementById("tg").addEventListener("click", function () {
-  const url = "https://x.com/market_age"; // Обновленная ссылка на Twitter
+  const url = "https://x.com/Medievalmarket_"; // Обновленная ссылка на Twitter
   window.open(url, '_blank'); // Открываем в новой вкладке
 });
 
