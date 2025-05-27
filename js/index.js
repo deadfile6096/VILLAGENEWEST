fbd6f4c809d3a0008605e6fe49f44397e0523b47
deadfile6096 <fap96@yandex.ru>
2025-05-27 22:57:12 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 document.getElementById("tg").addEventListener("click", function () {
-  const url = "https://x.com/Medievalmarket_"; // Обновленная ссылка на Twitter
+  const url = "https://x.com/TavernOfTrade"; // Обновленная ссылка на Twitter
   window.open(url, '_blank'); // Открываем в новой вкладке
 });
 
